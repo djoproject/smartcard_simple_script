8dff2f50a8a4cb9ed3f737b22067f2685aed277a
Bernard Paulus <bprecyclebin@gmail.com>
2013-12-11 15:07:33 +0100

    made code part runnnable

--- pcsc/rfid/acr122_v1/ultralight_ultralightC/code_part.py
+++ pcsc/rfid/acr122_v1/ultralight_ultralightC/code_part.py
@@ -17,14 +17,14 @@ connection = r[0].createConnection()
 connection.connect()
 
 ### PART 2 (list function) ###
->>>l = [11, 22, 33]
->>>l = l + [4]
->>>l
-[1, 2, 3, 44]
->>l[0]
-11
->>l[3]
-44
+#>>>l = [11, 22, 33]
+#>>>l = l + [4]
+#>>>l
+#[1, 2, 3, 44]
+#>>l[0]
+#11
+#>>l[3]
+#44
 
 ### PART 3 (get firmware version) ###
 data, sw1, sw2 = connection.transmit([0xff,0x0,0x48,0x1,0x0])
@@ -35,30 +35,30 @@ print chr(sw1)
 print chr(sw2)
 
 ### PART 4 (polling) ###
-    #send tag request
-    data, sw1, sw2 = connection.transmit([0xff, 0x0, 0x0, 0x0, 0x4, 0xd4, 0x4a, 0x2, 0x0, 0x0])
+#send tag request
+data, sw1, sw2 = connection.transmit([0xff, 0x0, 0x0, 0x0, 0x4, 0xd4, 0x4a, 0x2, 0x0, 0x0])
 
-    #check error code
-    if sw1 != 0x61:
-        print "polling error"
-        exit()
+#check error code
+if sw1 != 0x61:
+    print "polling error"
+    exit()
 
-    #get polling answer
-    data, sw1, sw2 = connection.transmit([0xff,0xC0,0x0,0x0,sw2])
-    
-    #check error code
-    if sw1 != 0x90 and sw2 != 0x00:
-        print "get Response error"
-        exit()
+#get polling answer
+data, sw1, sw2 = connection.transmit([0xff,0xC0,0x0,0x0,sw2])
 
-    if len(data) < 3:
-        print "not enought data"
-        exit()
-    
-    #is there at least one tag on the reader ?
-    if data[2] < 1:
-        print "no tag on the reader"
-        exit()
+#check error code
+if sw1 != 0x90 and sw2 != 0x00:
+    print "get Response error"
+    exit()
+
+if len(data) < 3:
+    print "not enought data"
+    exit()
+
+#is there at least one tag on the reader ?
+if data[2] < 1:
+    print "no tag on the reader"
+    exit()
 
 ### PART 5 (transfert function) ###
 def transfer(connection, tag_apdu):
@@ -92,17 +92,18 @@ def transfer(connection, tag_apdu):
     return data[3:]
 
 ### PART 6 (hexa function) ###
->>>hex(55)
-'0x37'
->>>0x37
-55
->>>int("0x37", 16)
-0x55
+#>>>hex(55)
+#'0x37'
+#>>>0x37
+#55
+#>>>int("0x37", 16)
+#0x55
 
 
 ### PART 7 (read data) ###
 sector_to_read = 0x0
 data = transfer(connection, [0x30, sector_to_read])
+print data
 
 ### PART 8 (write data) ###
 sector_to_write = 0x4
